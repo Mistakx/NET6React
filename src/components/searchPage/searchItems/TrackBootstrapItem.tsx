b78ac7547d22457132718d7d6245d0ad0e21f1cd
Maurício Camacho <joaocamacho91@gmail.com>
2022-05-05 21:43:40 +0100

    componente da pesquisa texto hover flow

--- src/components/searchPage/searchItems/TrackBootstrapItem.tsx
+++ src/components/searchPage/searchItems/TrackBootstrapItem.tsx
@@ -18,22 +18,22 @@ function TrackBoostrapItem(props: TrackItemComponentProperties): JSX.Element {
 
     return (
 
-        <div className="col-md-3">
+        <div className="col-md-3 col-6 position-relative">
             <div className="clickable card bg-dark"
                  onClick={() => {
                      setCurrentPlayerToClickedItem()
                  }}
                  style={{backgroundSize: "100% 100%", backgroundImage: "url(" + props.item.thumbnailUrl + ")"}}>
                 <div className="card-img-overlay text-end">
-                    <h5 className="card-title text-uppercase">{props.item.title}</h5>
-                    <p className="card-text">{props.item.albumName}</p>
-                    <p className="card-text">{props.item.creator}</p>
-                    <p className="card-text">{props.item.createdAt}</p>
-                    <button className="btn btn-sm" type="button"
-                            data-bs-toggle="modal" data-bs-target="#exampleModal"><i
-                        className='bx bx-plus'></i></button>
+                    <h5 className="card-title text-uppercase text-truncate">{props.item.title}</h5>
+                    <p className="card-text text-truncate">{props.item.albumName}</p>
+                    <p className="card-text text-truncate">{props.item.creator}</p>
+                    {/* <p className="card-text text-truncate">{props.item.createdAt}</p> */}
+                    </div>
                 </div>
-            </div>
+                <button className="btn btn-sm btn-add" type="button"
+                    data-bs-toggle="modal" data-bs-target="#exampleModal"><i
+                    className='bx bx-plus'></i></button>
         </div>
 
 
