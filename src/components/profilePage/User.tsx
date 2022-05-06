56e3e8b635fe55e594cc749ad9351f1eea641fad
Maurício Camacho <joaocamacho91@gmail.com>
2022-05-06 20:45:22 +0100

    dropdown, icon camera

--- src/components/profilePage/User.tsx
+++ src/components/profilePage/User.tsx
@@ -6,13 +6,20 @@ import '../../styles/style.css';
 function User(): JSX.Element {
 
     return (
-        <div className="col-md-4">
-
+        <div className="col-md-4 position-relative">
             <div className="align-items-stretch mb-4 " data-aos="zoom-in" data-aos-delay="100">
                 <div className="icon-box iconbox-blue rounded">
-                    <h4 className="text-white">User name</h4>
+                    <h4 className="text-white">Antero</h4>
                     <img src="https://image.api.playstation.com/vulcan/img/rnd/202109/0114/ql9sjqcZguB1Iz0LUJcKN3yG.png" alt=""
-                         className="img-fluid rounded-circle img-centered"/>
+                        className="img-fluid rounded-circle img-centered"/>
+                    <button className="btn rounded position-absolute top-50 start-50 translate-middle mt-5">
+                        <i className='bx bx-camera'></i>
+                    </button>
+                </div>
+                <div className="options-top mr-5 mb-5">
+                    <button className="btn text-white">
+                        <i className='bx bx-edit-alt' ></i>
+                    </button>
                 </div>
             </div>
         </div>
