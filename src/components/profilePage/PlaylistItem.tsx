e9e0f2ceb9c1977294e99c2f3d5fa1dd4c5f30e3
Maurício Camacho <joaocamacho91@gmail.com>
2022-05-06 20:22:22 +0100

    albuns perfil

--- src/components/profilePage/PlaylistItem.tsx
+++ src/components/profilePage/PlaylistItem.tsx
@@ -5,38 +5,30 @@ function PlaylistItem(): JSX.Element {
 
     return (
 
-        <div className="col-md-4">
-            <div className="card bg-dark rounded"
-                 style={{backgroundImage: "url(https://cdn.pixabay.com/photo/2016/03/28/09/36/music-1285165_960_720.jpg)"}}>
-                <div className="card-img-overlay text-end rounded">
+        <div className="col-md-6 col-sm-6 col-lg-4 col-6 position-relative scale">
+            <div className="card bg-dark"
+            style={{backgroundImage: "url(https://cdn.pixabay.com/photo/2016/03/28/09/36/music-1285165_960_720.jpg)"}}>
+                <div className="card-img-overlay text-end">
                     <h5 className="card-title text-uppercase">Nome Música</h5>
                     <p className="card-text">Tipo album</p>
                     <p className="card-text">Artista</p>
-
-
-                    <div className="options">
-
-                        <button className="btn btn-primary"
-                                type="button"
-                                data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">
-                            <i className='bx bx-play'></i>
+                </div>
+                <div className="options-dropdown">
+                    <div className="btn-group">
+                        <button type="button" className="btn dropdown-toggle-split" 
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                            <i className='bx bx-dots-horizontal-rounded'></i>
+                            <span className="visually-hidden">Toggle Dropdown</span>
                         </button>
-
-                        <a href="player.html"
-                           className="btn btn-primary">
-                            <i className='bx bx-plus'>
-                            </i>
-                        </a>
-
+                        <ul className="dropdown-menu dropdown-menu-dark">
+                            <li><a className="dropdown-item" href="#">Editar</a></li>
+                            <li><a className="dropdown-item" href="#">Alterar nome</a></li>
+                        </ul>
                     </div>
-
                 </div>
             </div>
         </div>
-
     )
-
 }
 
 export default PlaylistItem
