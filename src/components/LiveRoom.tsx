4953fc8a0f6af6d4326091231237bce0864a1ab2
Mistakx <ade9@live.com.pt>
2022-06-30 01:47:39 +0100

    Online people button only appears when logged in

--- src/components/LiveRoom.tsx
+++ src/components/LiveRoom.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "aos/dist/aos.css";
 import AOS from "aos";
 import {useNavigate} from "react-router-dom";
-import { HubConnectionSingleton } from "utils/HubConnectionSingleton";
+import {HubConnectionSingleton} from "utils/HubConnectionSingleton";
 import AlertStore from "stores/AlertStore";
-import { ConnectToHubDto } from "models/backendRequests/HubConnections/ConnectToHubDto";
+import {ConnectToHubDto} from "models/backendRequests/HubConnections/ConnectToHubDto";
 
 function LiveRoom(): JSX.Element {
 
@@ -25,31 +25,43 @@ function LiveRoom(): JSX.Element {
     }, []);
 
     useEffect(() => {
-        window.onbeforeunload = function(){
-            let sendParams : ConnectToHubDto = {
-                sessionToken : window.sessionStorage.getItem("sessionToken"),
-                hubConnectionId : hubConnection.connectionId
+        window.onbeforeunload = function () {
+            let sendParams: ConnectToHubDto = {
+                sessionToken: window.sessionStorage.getItem("sessionToken"),
+                hubConnectionId: hubConnection.connectionId
             }
 
             hubConnection.send("UserDisconnected", sendParams);
             hubConnection.stop()
-          }
+        }
     }, []);
 
     let liveRoom;
     if (localStorage.getItem("sessionToken")) {
-        liveRoom = 
-            
+        liveRoom =
 
+            <div className="position-relative">
+                <div className="live-room">
+                    <a className="intro-banner-vdo-play-btn green-sinal" type="button"
+                       data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"
+                       title="See people online">
+                        <i className="glyphicon glyphicon-play whiteText" aria-hidden="true"></i>
+                        <span className="ripple green-sinal"></span>
+                        <span className="ripple green-sinal"></span>
+                        <span className="ripple green-sinal"></span>
+                    </a>
+                </div>
                 <div className="offcanvas offcanvas-end" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
                     <div className="offcanvas-header">
                         <h5 id="offcanvasRightLabel">Live Room</h5>
-                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
 
                         <div className="list-group">
-                            <a href="#" className="list-group-item list-group-item-action" aria-current="true" data-aos="fade-left" data-aos-duration="3000">
+                            <a href="#" className="list-group-item list-group-item-action" aria-current="true"
+                               data-aos="fade-left" data-aos-duration="3000">
                                 <div className="row">
 
                                     <div className="col-3">
@@ -57,7 +69,8 @@ function LiveRoom(): JSX.Element {
                                         <div className="image_outer_container">
                                             <div className="green_icon"></div>
                                             <div className="image_inner_container">
-                                                <img className="img-fluid" src="https://i.pinimg.com/originals/43/96/61/439661dcc0d410d476d6d421b1812540.jpg"/>
+                                                <img className="img-fluid"
+                                                     src="https://i.pinimg.com/originals/43/96/61/439661dcc0d410d476d6d421b1812540.jpg"/>
                                             </div>
                                         </div>
 
@@ -75,25 +88,11 @@ function LiveRoom(): JSX.Element {
 
                     </div>
                 </div>
+            </div>
 
     }
 
-    return (
-
-        <div className="position-relative">
-            <div className="live-room">
-                <a className="intro-banner-vdo-play-btn green-sinal" type="button"
-                data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" title="See people online">
-                    <i className="glyphicon glyphicon-play whiteText" aria-hidden="true"></i>
-                    <span className="ripple green-sinal"></span>
-                    <span className="ripple green-sinal"></span>
-                    <span className="ripple green-sinal"></span>
-                </a>
-            </div>
-            {liveRoom}
-        </div>
-
-    )
+    return (<> {liveRoom}</>)
 
 }
 
