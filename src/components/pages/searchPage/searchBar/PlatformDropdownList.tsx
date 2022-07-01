2c9e346886c4ece1cb07169b5c6cb800398c6b57
Maurício Camacho <joaocamacho91@gmail.com>
2022-07-01 23:23:12 +0100

    icon mixcloud dropdown

--- src/components/pages/searchPage/searchBar/PlatformDropdownList.tsx
+++ src/components/pages/searchPage/searchBar/PlatformDropdownList.tsx
@@ -125,7 +125,7 @@ function PlatformDropdownList(props: PlatformDropdownListProperties): JSX.Elemen
                     e.preventDefault()
                     setSelectedSearch(MixcloudSearchTrack.getInstance())
                 }}>
-                    <i className='bx bxl-mixcloud'></i>
+                    <i className="fa-brands fa-mixcloud"></i>
                 </div>
             </li>
 
